b0c8b8956d0e942749fa3c8d034b18e31812bfca
Vmagalona15 <100141653+Vmagalona15@users.noreply.github.com>
2022-02-21 22:19:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+        title>HOMEPAGE</title>
+  </head>
+  <body>
+  <h1>WELCOME TO MY WEBSITE</h1>
+  <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+  </ul>
+  </body>
+</html>
