bb655d409136625ac06fdc8600f3cfc45cc2f535
Vmagalona15 <100141653+Vmagalona15@users.noreply.github.com>
2022-02-21 22:34:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <h1>WELCOME TO MY WEBSITE</h1>
   <ul>
       <li><a href="index.php">HOME</a></li>
-      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+      <li><a href="/LT2/HP.html">LEARNING TASK 2</a></li>
   </ul>
   </body>
 </html>
