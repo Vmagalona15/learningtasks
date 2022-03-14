6031eff1fc5a0c2e235e477b92f945e2ad9b6d6b
Vmagalona15 <100141653+Vmagalona15@users.noreply.github.com>
2022-03-14 23:49:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <ul>
       <li><a href="index.php">HOME</a></li>
       <li><a href="/LT2/HP.html">LEARNING TASK 2</a></li>
-      <li><a href="/LT2/Favorites.html">LEARNING TASK 3</a></li>
+      <li><a href="/LT3/Favorites.html">LEARNING TASK 3</a></li>
   </ul>
   </body>
 </html>
